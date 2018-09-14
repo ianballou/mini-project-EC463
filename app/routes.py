04f3685390e129469dfd2ef08c6c749904c0a06e
Izhmash <ianballou67@gmail.com>
2018-09-14 16:04:28 -0400

    user auth working, need registration.

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 from app.models import User
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    test_user = {'username': 'ian'}
-    return render_template('index.html', title='Home', user=test_user)
+    return render_template('index.html', title='Home')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,7 +22,11 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        #return redirect((url_for(next_page)[0])[1:])
+        return redirect('index')  # XXX placeholder!
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
